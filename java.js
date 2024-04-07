f8d8c7f08efe17bb325b54fc0ad0e2a33beac81d
CASIMIR <142940409+casimirbole@users.noreply.github.com>
2024-04-07 15:53:44 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,50 @@
+
+function toggleblock() {
+    var bloc =document.getElementById("monbloc");
+
+    if (bloc.style.display==="none") {
+        bloc.style.display ="block"; 
+    } 
+}
+
+
+
+
+
+function toggle() {
+    var bloc =document.getElementById("monbloc");
+
+    if (bloc.style.display==="block") {
+        bloc.style.display ="none"; 
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var button =document.getElementById('casi');
+// button.addEventListener('click',function(){
+//     button.style.backgroundColor='blue';
+//     button.style.justifyContent='80px';
+//     button.style.marginLeft='400px';
+//     button.style.backgroundColor='yellow';
+//     button.style.fontSize='80px';
+//     button.style.overscrollBehaviorX='50px';
